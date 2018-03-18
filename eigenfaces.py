99e27e581b6cbc2c3eb0ca8180e204771025ed9f
jonas@valfridsson.net <jonas@valfridsson.net>
2018-03-18 21:57:31 +0100

    Eigenfaces looks reasonable, only classification left

--- eigenfaces.py
+++ eigenfaces.py
@@ -3,6 +3,8 @@ from random import choice
 from numpy import array, mean, zeros, sum, fromstring, full, vectorize, argsort
 from numpy.linalg import eig, norm
 from pandas import read_csv
+from matplotlib import pyplot as plt
+from time import time
 
 
 def get_training_and_test_data():
@@ -13,10 +15,12 @@ def get_training_and_test_data():
     for index, img in enumerate(data["Image"]):
         images[index] = fromstring(img, dtype=int, sep=" ")
 
-    return (choose_training_images(images), choose_training_images(images, cardinality=100))
+    return (choose_training_images(images),
+            choose_training_images(images, cardinality=100))
 
 
-def dimensionality_reduction(matrix, reduction_filter=full((3, 3), 1 / 9), image_shape=(96, 96)):
+def dimensionality_reduction(matrix, reduction_filter=full((3, 3), 1 / 9),
+                             image_shape=(96, 96)):
     """Reduce dimension of a image."""
     matrix = matrix.reshape(image_shape)
     (x_dim, y_dim) = reduction_filter.shape
@@ -57,15 +61,62 @@ def choose_training_images(images, cardinality=500):
     return training_images
 
 
+def vector_to_image(vector, image_shape):
+    """Imagize a vector."""
+    im_dim = vector.reshape(image_shape)
+    image = zeros((image_shape[0], image_shape[1], 3))
+
+    kind_of_abs = lambda x: x if x > 0 else 255
+    for xi, row in enumerate(im_dim):
+        for yi, cell in enumerate(row):
+            image[xi, yi] = [kind_of_abs(im_dim[xi, yi]) / 256] * 3
+
+    return image
+
+
 class Eigenclassifier(object):
     """Classifier scope."""
 
-    def __init__(self, base_dim=5, threshold=lambda x: x):
+    def __init__(self, base_dim=5, threshold=lambda x: x, verbose=True):
         """Constructor."""
         self.base_dim = base_dim
         self.threshold = threshold
         self.eigen_base = None
         self.mean_face = None
+        self.covariance = None
+        self.expected_values = None
+        self.verbose = verbose
+
+    def show_principal_component(self, shape=(32, 32)):
+        """Show the eigen face of the principal component."""
+        if self.eigen_base is None:
+            sys.stderr.write(
+                "Must build base before trying to show the classifier")
+            sys.exit(1)
+
+        fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4)
+
+        principal_component_0 = self.eigen_base[0]
+        principal_component_1 = self.eigen_base[1]
+        principal_component_2 = self.eigen_base[2]
+        principal_component_3 = self.eigen_base[3]
+
+        project_into_origin_space =\
+            lambda eigen: self.expected_values.T @ eigen\
+            + self.mean_face
+
+        pc_project_0 = project_into_origin_space(principal_component_0)
+        pc_project_1 = project_into_origin_space(principal_component_1)
+        pc_project_2 = project_into_origin_space(principal_component_2)
+        pc_project_3 = project_into_origin_space(principal_component_3)
+
+        ax1.imshow(vector_to_image(pc_project_0, shape))
+        ax2.imshow(vector_to_image(pc_project_1, shape))
+        ax3.imshow(vector_to_image(pc_project_2, shape))
+        ax4.imshow(vector_to_image(pc_project_3, shape))
+
+        plt.tight_layout()
+        plt.show()
 
     def build_base(self, training_data):
         """
@@ -79,18 +130,15 @@ class Eigenclassifier(object):
 
         where M is the mean vector
         """
+        time_stamp_prebuild = time()
         self.mean_face = mean(training_data, axis=0)
 
         """ This broadcasts the mean_vector through the matrix """
-        expected_values = training_data - self.mean_face
+        self.expected_values = training_data - self.mean_face
+        self.covariance = self.expected_values @ self.expected_values.T
 
-        """
-        The article says to calculate the covariance matrix
-        but i don't understand why or where it is used.
-        """
-        covariance = expected_values.T @ expected_values
-        (_, y_dim) = covariance.shape
-        (scalars, vectors) = eig(covariance)
+        (_, y_dim) = self.covariance.shape
+        (scalars, vectors) = eig(self.covariance)
 
         if len(scalars) < self.base_dim:
             print("Cannot build eigen base of " + self.base_dim
@@ -117,12 +165,17 @@ class Eigenclassifier(object):
             im_stripped = im_stripper(vector)
             self.eigen_base[index] = im_stripped / norm(im_stripped)
 
-
         """
         Perhaps there's something to gain by orthonormalizing
         the base? To do that apply repeated projection and subtraction
         i might implement it later if noone else does.
         """
+
+        if self.verbose:
+            print(
+                "base took {} seconds to build".format(
+                    time() - time_stamp_prebuild
+                ))
 
         return self.eigen_base
 
@@ -156,13 +209,17 @@ class Eigenclassifier(object):
         return base_projection
 
 
+time_stamp_pre_datafetch = time()
 (training_data, test_data) = get_training_and_test_data()
+print("took {} seconds to read data".format(time() - time_stamp_pre_datafetch))
+
+
+
 classifier = Eigenclassifier()
-
-
 base = classifier.build_base(training_data)
-print(base)
-print(classifier.predict(test_data[0]))
+classifier.show_principal_component()
+
+
 
 
 # print(images[0])
